0b232af844609e15bb85e6178f595f0e9d76cb4e
EC2 Default User <ec2-user@ip-172-31-27-223.us-east-2.compute.internal>
2018-12-09 05:35:52 +0000

    FIX fixed path to telegram process, added /wish method to telegram bot

--- node/app/child-processes/telegram/index.js
+++ node/app/child-processes/telegram/index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '';
+const token = '633707259:AAH7KSjTnRAUJtly1EXGwPDDwyRIHxQ6W0U';
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -8,6 +8,14 @@ bot.on('audio', (msg) => {
 
 });
 
-bot.on('text', (msg) => {
+//bot.on('text', (msg) => {
 //  process.send({ from, to, message, label, speaker });
+//});
+
+bot.onText(/\/wish/, (msg, matches) => {
+  console.log('Got message');
+
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Желаю тебе всего наилучшего');
 });
--- node/app/init-config.js
+++ node/app/init-config.js
@@ -11,18 +11,18 @@ const ttsPath = path.resolve(__dirname, './child-processes/tts/index.js');
 const ftpPath = path.resolve(__dirname, './child-processes/ftp/index.js');
 const messagesPath = path.resolve(__dirname, './child-processes/messages/index.js');
 const paintingPath = path.resolve(__dirname, './child-processes/painting/index.js');
-const telegramPath = path.resolve(__dirname, './child-processes/painting/index.js');
+const telegramPath = path.resolve(__dirname, './child-processes/telegram/index.js');
 
 module.exports = function() {
 	ttsProcess = child_process.fork(ttsPath);
 	// ftpProcess = child_process.fork(ftpPath);
-	// telegramProcess = child_process.fork(telegramPath);
+	 telegramProcess = child_process.fork(telegramPath);
 
 	// telegramProcess.on('message', sendToTTSProcess);
 
-	setTimeout(() => {
-		sendToTTSProcess({ speaker: 'Tatyana', from: 'Миша', to: 'Всем', message: 'Хэй хей! Как ваше ничего поросята?' });
-	}, 5000);
+//	setTimeout(() => {
+//		sendToTTSProcess({ speaker: 'Tatyana', from: 'Миша', to: 'Всем', message: 'Хэй хей! Как ваше ничего поросята?' });
+//	}, 5000);
 
 	// messagesProcess = child_process.fork(messagesPath);
 	// paintingProcess = child_process.fork(paintingPath);
