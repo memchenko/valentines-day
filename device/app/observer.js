a614d854ecadddbdc4e06835267611cae6e75160
memchenko <mikhail.emchenko@gmail.com>
2018-12-16 16:51:07 +0400

    device observer

--- device/app/observer.js
+++ device/app/observer.js
@@ -1,18 +1,37 @@
-const johnnyFive = require('johnny-five');
+const five = require('johnny-five');
+const board = new five.Board();
 
 let eventEmitter;
 
 const PINS = {
   MOUTH: 1,
   EYES: [2,3],
-  ULTRASONIC: 4
+  ULTRASONIC: 4,
+  MOTOR: 5
 };
 
 const SERIAL_PINS = {
-  TX: 5,
-  RX: 6
+  TX: 6,
+  RX: 7
 };
 
+board.on('ready', () => {
+  const proximity = new five.Proximity({
+    controller: "HCSR04",
+    pin: PINS.ULTRASONIC
+  });
+
+  proximity.on("data", function() {
+    console.log("Proximity: ");
+    console.log("  cm  : ", this.cm);
+    console.log("  in  : ", this.in);
+    console.log("-----------------");
+  });
+
+  proximity.on("change", function() {
+    console.log("The obstruction has moved.");
+  });
+});
 
 module.exports = (_eventEmitter) => {
   eventEmitter = _eventEmitter;
